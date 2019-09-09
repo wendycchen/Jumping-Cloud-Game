7f9dd4049a2eb47bd5405fdd674bae5e11af2c8d
wendycchen <55068125+wendycchen@users.noreply.github.com>
2019-09-08 19:28:18 -0600

    Add files via upload

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //void Update()
+    //{
+    // if (Input.GetKeyDown(KeyCode.LeftArrow))
+    //    {
+    //        transform.Translate(-1.5f, 0, 0);
+    //    }
+    // if (Input.GetKeyDown(KeyCode.RightArrow))
+    //    {
+    //        transform.Translate(1.5f, 0, 0);
+    //    }
+    //}
+
+    public void LButtonDown()
+    {
+        transform.Translate(-1.5f, 0, 0);
+    }
+
+    public void RButtonDown()
+    {
+        transform.Translate(1.5f, 0, 0);
+    }
+}
